dca3775b259da16080e446a17021bb0e404120f6
Yue Wang <wy2007121@gmail.com>
2018-06-17 10:57:08 -0500

    Updated z3 installer

---
+++ installer/targets/z3/install.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ ! -f /usr/local/bin/z3 ]
+then
+	git clone https://github.com/Z3Prover/z3.git ~/.z3
+	cd ~/.z3
+	python scripts/mk_make.py
+	cd build
+	make
+	sudo make install 
+fi
