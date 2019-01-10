6ce8d218a0b266dcf7b75f58214da86f1d6d9bac
Juan David Hernandez Vega <juandhv@rice.edu>
2019-01-10 16:22:32 -0600

    MoveIt and OMPL from PPA repos

--- installer/targets/ros-moveit_experimental/install.bash
+++
@@ -1,8 +0,0 @@
-if [ ! -d $KAVRAKILAB_SYSTEM_DIR/src/moveit ]
-then
-    kavrakilab-install-target ros-moveit
-    kavrakilab-install-target ros-moveit_msgs
-    kavrakilab-install-target ros-moveit_resources
-    kavrakilab-install-target ros-ompl
-fi
-
